8fce3d64561d462a55029034f9667aa0058681f5
Lucas Soares <114198539+oilucassoares@users.noreply.github.com>
2023-04-02 14:40:38 -0300

    Calculadora
    Calculadora feita com HTML-CSS-JavaScript

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const n1 = document.querySelector(".n1");
+const n2 = document.querySelector(".n2");
+const seletor = document.querySelector(".operacoes");
+let resultado = document.querySelector(".resultado");
+
+seletor.style.padding="10px"
+n1.style.width="80px"
+n2.style.width="80px"
+n1.style.height="33px"
+n2.style.height="33px"
+
+/*change é "escolha", já o "keyup" é para quando o usuário TIRAR o dedo do teclado, daí atualiza em tempo real.*/
+seletor.addEventListener("change", calcular);
+n1.addEventListener("keyup", calcular);
+n2.addEventListener("keyup", calcular);
+
+function calcular(){
+    const v1 = Number(n1.value);
+    const v2 = Number(n2.value);
+    const operacoes = seletor.value;
+    if(operacoes==="+")
+        resultado.innerHTML = v1 + v2;
+    if(operacoes==="-")
+        resultado.innerHTML = v1 - v2;
+    if(operacoes==="*")
+        resultado.innerHTML = v1 * v2;
+    if(operacoes==="/")
+        resultado.innerHTML = v1 / v2;
+}
